7b206f9f067109c84e7f9692d0bb3ed05a5d4c5f
Anh Le Hoang <lehoanganh25991@gmail.com>
2019-03-05 10:55:56 +0700

    Dump GraphQL json schema > typescript

---
+++ gql-2-typescript.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const { generateNamespace } = require('@gql2ts/from-schema');
+
+// const gqlSchema = fs.readFileSync(path.join(__dirname, 'gql-schema.graphql'), { encoding: 'utf8' });
+const gqlSchemaJson = require('./export-json-schema.json');
+const gqlTypes = generateNamespace('gateway', gqlSchemaJson);
+fs.writeFileSync(path.join(__dirname, 'gql.d.ts'), gqlTypes);
